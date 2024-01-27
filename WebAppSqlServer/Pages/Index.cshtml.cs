d82b79328cafb2fd4e4020d704536a888a71d6de
monzondeveloper <monzondeveloper@gmail.com>
2024-01-27 14:20:16 -0300

    First Commit
    This commit has the Project itself with Azure Sql Server

---
+++ WebAppSqlServer/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppSqlServer.Models;
+using WebAppSqlServer.Services;
+
+namespace WebAppSqlServer.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> products;
+
+
+        public void OnGet()
+        {
+            var service = new ProductService();
+            products = service.GetProducts();
+        }
+    }
+}
---
+++ WebAppSqlServer/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SqlClient;
+using WebAppSqlServer.Models;
+
+namespace WebAppSqlServer.Services
+{
+    public class ProductService
+    {
+        private static string dbServer = "appserver5000.database.windows.net";
+        private static string dbUser = "mmonzon";
+        private static string dbPassword = "Azure@123";
+        private static string dbName = "appdb";
+
+     
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+
+            var products = new List<Product>();
+
+            string statement = "SELECT ProductId, ProductName, Quantity FROM Products";
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(statement, conn);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read()) 
+                {
+                    Product product = new Product
+                    {
+                        ProductId = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+                    };
+
+                    products.Add(product);
+                }
+            }
+
+            conn.Close();
+            conn.Dispose();
+
+            return products;
+        }
+
+        private SqlConnection GetConnection()
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = dbServer;
+            builder.UserID = dbUser;
+            builder.Password = dbPassword;
+            builder.InitialCatalog = dbName;
+
+            return new SqlConnection(builder.ConnectionString);
+        }
+    }
+}
